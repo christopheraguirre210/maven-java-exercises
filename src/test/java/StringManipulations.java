a4c62314b1e5b59f137be44d3cdd1804815204f9
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-12-12 15:44:43 -0600

    updated maven-exercise repo

---
+++ src/test/java/StringManipulations.java
@@ -0,0 +1,26 @@
+import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
+
+public class StringManipulations {
+    public static void main(String[] args) {
+
+        textCheck();
+
+
+    }
+
+    public static void textCheck(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give me some input please.");
+
+        String userInput = scanner.nextLine();
+
+        if(StringUtils.isNumeric(userInput)){
+            System.out.println("That is not a text input");
+        }else{
+            System.out.println("userInput = " + userInput);
+        }
+    }
+
+}
